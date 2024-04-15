5e51303991ec53b7cdb8e340f3e140b2e5f09ff3
HamedSadim1 <hamid.sadim@outlook.com>
2024-04-15 18:12:37 +0200

    add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = ({ children }: { children: React.ReactNode }) => (
+  <header className="header">{children}</header>
+);
+
+export const HeaderTop = ({ children }: { children: React.ReactNode }) => (
+  <div className="header__top">{children}</div>
+);
