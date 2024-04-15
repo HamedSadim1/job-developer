0e24ae96acfd0abf40a3fd047897286472cad51b
HamedSadim1 <hamid.sadim@outlook.com>
2024-04-15 17:35:55 +0200

    add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,21 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <small>
+        <p>
+          © Copyright by
+          <a href="https://bytegrad.com" target="_blank">
+            ByteGrad.com
+          </a>
+          . Intended for learning or your portfolio.
+        </p>
+      </small>
+
+      <p>
+        <span className="u-bold">109573</span> total jobs available
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
