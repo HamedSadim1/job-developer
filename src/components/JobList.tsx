2f3a3fb05ce6c7bcec88a1387d6566af373d647a
HamedSadim1 <hamid.sadim@outlook.com>
2024-04-16 18:47:57 +0200

    refactor index css & joblist component

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -1,4 +1,3 @@
-import { jobListings } from "../lib/constants";
 import { useActiveIdContext } from "../lib/hooks";
 import { JobItem } from "../lib/types";
 import JobListItem from "./JobListItem";
