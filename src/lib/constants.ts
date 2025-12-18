a913ff3348579c21cc1f9c12727b40283427cb7d
HamedSadim1 <hamid.sadim@outlook.com>
2025-12-18 18:44:45 +0100

    Add fallback for BASE_API_URL in production

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,8 @@
 /**
  * The base API URL for making requests to the remote server.
  */
-export const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
+export const BASE_API_URL =
+  import.meta.env.VITE_BASE_API_URL ||
+  "https://bytegrad.com/course-assets/projects/rmtdev/api/data";
 
 export const RESULTS_PER_PAGE = 7;
